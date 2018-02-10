ba34793bcf02d85b364700e30784b4c64edc8fde
dtmkeng <kang081@hotmail.con>
2018-02-10 21:15:18 +0700

    init dev

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1>RECAN..</h1>
+          <h1>RECAN</h1>
           <Routes/>
       </div>
     );
